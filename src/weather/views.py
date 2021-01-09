f34ae7fac3bcd64e02a005283979f2a66325dd8f
SemihDurmus <semihdurmus@yahoo.com>
2021-01-09 19:10:28 +0100

    Pulled data from APi and displayed on index.html

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
+
 
 def index(request):
-    return render(request, "weather/index.html")
+    url = config("BASE_URL")
+    city = "Berlin"
+    r = requests.get(url.format(city))
+    returned_data = r.json()
+    # print(type(returned_data))
+    # pprint(returned_data)
+    context = {
+        "city": city,
+        "temp": returned_data["main"]["temp"],
+        "description": returned_data["weather"][0]["description"],
+        "icon": returned_data["weather"][0]["icon"],
+    }
+    return render(request, "weather/index.html", context)
