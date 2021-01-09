6006b67caff03733826966adfbde11885958e728
SemihDurmus <semihdurmus@yahoo.com>
2021-01-09 19:57:11 +0100

    Update function in views and display on home page

--- src/weather/views.py
+++ src/weather/views.py
@@ -2,19 +2,28 @@ from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
+from .models import City
 
 
 def index(request):
     url = config("BASE_URL")
-    city = "Berlin"
-    r = requests.get(url.format(city))
-    returned_data = r.json()
-    # print(type(returned_data))
-    # pprint(returned_data)
+
+    cities = City.objects.all()
+    city_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city))
+        returned_data = r.json()
+
+        weather_data = {
+            "city": city.name,
+            "temp": returned_data["main"]["temp"],
+            "description": returned_data["weather"][0]["description"],
+            "icon": returned_data["weather"][0]["icon"],
+        }
+        city_data.append(weather_data)
+    pprint(city_data)
     context = {
-        "city": city,
-        "temp": returned_data["main"]["temp"],
-        "description": returned_data["weather"][0]["description"],
-        "icon": returned_data["weather"][0]["icon"],
+        "city_data": city_data
     }
     return render(request, "weather/index.html", context)
